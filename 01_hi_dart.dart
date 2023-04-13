a4051324e1877cdcf42302407b1f1c75556c7cdd
LeopoldoIII <nathan.rx@gmail.com>
2023-04-13 13:48:45 -0600

    first commit dart

---
+++ 01_hi_dart.dart
@@ -0,0 +1,8 @@
+void main() {
+  // var myName = 'MyName';
+  // late final myName;
+  // const myName = 'Myname';
+  String myName = 'MyName';
+  print('Hola $myName');
+  print('Hola ${myName.toUpperCase()}');
+}
