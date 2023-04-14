3de9b408477c8af5cb2e79c3606ddeca17e040d5
LeopoldoIII <nathan.rx@gmail.com>
2023-04-13 22:43:00 -0600

    dymamic

--- 02_variables.dart
+++ 02_variables.dart
@@ -5,11 +5,20 @@ void main() {
   final abilities = <String>['impostor'];
   final List<String> sprites = ['ditto/front.png', 'ditto/back.png'];
 
+// dynamic == null
+  dynamic errorMessage = 'Hola';
+  errorMessage = true;
+  errorMessage = [1, 2, 3, 4, 5, 6];
+  errorMessage = {1, 2, 3, 4, 5, 6};
+  errorMessage = () => true;
+  errorMessage = null;
+
   print("""
   $pokemon
   $hp
   $isAlive
   $abilities
   $sprites
+  $errorMessage
 """);
 }
