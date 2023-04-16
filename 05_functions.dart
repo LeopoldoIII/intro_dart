4372a9c766c60852076043ec18aa2d24805c5236
LeopoldoIII <nathan.rx@gmail.com>
2023-04-16 14:56:10 -0600

    parameters

--- 05_functions.dart
+++ 05_functions.dart
@@ -2,13 +2,18 @@ void main() {
   print(greetEveryone());
   print('Suma: ${addTwoNumbers(10, 20)}');
   print('Suma: ${addTwoNumbersLab(10, 20)}');
+  print(greetPerson(name: 'Fernando', message: 'Hi'));
 }
 
 String greetEveryone() => 'Hello everyone!';
 
 int addTwoNumbers(int a, [int b = 0]) {
-  // b ??= 0 
+  // b ??= 0
   return a + b;
 }
 
 int addTwoNumbersLab(int a, int b) => a + b;
+
+String greetPerson({required String name, String message = 'Hola'}) {
+  return '$message,  $name';
+}
