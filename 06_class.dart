69ff92fcbfeca99324816fc3f9874bdd822ee100
LeopoldoIII <nathan.rx@gmail.com>
2023-04-16 15:13:31 -0600

    override

---
+++ 06_class.dart
@@ -0,0 +1,29 @@
+void main() {
+  final wolverine = Hero(name: 'Logan', power: 'Regeneración');
+  print(wolverine);
+  print(wolverine.name);
+  print(wolverine.power);
+}
+
+class Hero {
+  String name;
+  String power;
+
+  Hero({required this.name, this.power = 'No power'});
+
+  // Hero(String pName, String pPower) {
+  //   name = pName;
+  //   power = pPower;
+  // }
+
+
+
+@override
+  String toString() {
+    // TODO: implement toString
+    return '$name - $power';
+  }
+
+
+
+}
