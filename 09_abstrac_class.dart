80f0d923e0cbb047772825852e58918f6719f313
LeopoldoIII <nathan.rx@gmail.com>
2023-04-16 19:32:30 -0600

    extends

---
+++ 09_abstrac_class.dart
@@ -0,0 +1,35 @@
+void main() {
+  final windplant = WindPlant(initialEnergy: 11);
+
+  print(windplant);
+
+  print('Wind:  ${chargePhone(windplant)}');
+}
+
+double chargePhone(EnergyPlant plant) {
+  if (plant.energyLeft < 10) {
+    throw Exception('Not enough energy');
+  }
+  return plant.energyLeft - 10;
+}
+
+enum PlantType { nuclear, wind, water }
+
+abstract class EnergyPlant {
+  double energyLeft;
+  PlantType type;
+
+  EnergyPlant({required this.energyLeft, required this.type});
+
+  void consumeEnergy(double amount);
+}
+
+class WindPlant extends EnergyPlant {
+  WindPlant({required double initialEnergy})
+      : super(energyLeft: initialEnergy, type: PlantType.wind);
+
+  @override
+  void consumeEnergy(double amount) {
+    energyLeft -= amount;
+  }
+}
